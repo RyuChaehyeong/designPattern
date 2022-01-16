668c47b7c6c8db9d8a31bfcb9d1c03c3df8ab790
RyuChaehyeong <fluid15@naver.com>
2022-01-16 21:39:07 +0900

    Init commit

---
+++ src/Iterator/BoolShelfIterator.java
@@ -0,0 +1,30 @@
+package Iterator;
+
+import java.util.Iterator;
+
+public class BoolShelfIterator implements Iterator {
+
+    private BookShelf bookShelf;
+    private int index;
+
+    public BoolShelfIterator(BookShelf bookShelf) {
+        this.bookShelf = bookShelf;
+        this.index = 0;
+    }
+
+    @Override
+    public boolean hasNext() {
+        if (index < bookShelf.getLength()) {
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+    @Override
+    public Object next() {
+        Book book = bookShelf.getBookAt(index);
+        index++;
+        return book;
+    }
+}
---
+++ src/입출금내역분석기/BankTransactionAnalyzerSimple.java
@@ -0,0 +1,28 @@
+package 입출금내역분석기;
+
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.List;
+
+public class BankTransactionAnalyzerSimple {
+
+    private static final String RESOURCES = "src/main/resources";
+
+    public static void main(final String... args) throws Exception {
+
+        //모든 거래 내역의 합 계산하기
+        final Path path = Paths.get(RESOURCES + args[0]);
+        final List<String> lines = Files.readAllLines(path);
+        double total = 0d;
+
+        for (final String line : lines) {
+            final String[] columns = line.split(",");
+            final double amount = Double.parseDouble(columns[1]);
+            total += amount;
+        }
+
+        System.out.println("The total for all transactions is " + total);
+
+    }
+}
