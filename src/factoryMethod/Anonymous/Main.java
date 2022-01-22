a8e18ee7c71eef3a1ae5dd30f0e6fd02db186647
RyuChaehyeong <fluid15@naver.com>
2022-01-22 21:27:03 +0900

    [Design Patter] Factory Method

---
+++ src/factoryMethod/Anonymous/Main.java
@@ -0,0 +1,18 @@
+package factoryMethod.Anonymous;
+
+import factoryMethod.framework.Factory;
+import factoryMethod.framework.Product;
+import factoryMethod.idcard.IDCardFactory;
+
+public class Main {
+    public static void main(String[] args) {
+        Factory factory = new IDCardFactory();
+        Product card1 = factory.create("홍길동");
+        Product card2 = factory.create("이순신");
+        Product card3 = factory.create("유관순");
+
+        card1.use();
+        card2.use();
+        card3.use();
+    }
+}
