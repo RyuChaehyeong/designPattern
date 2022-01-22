4f14c5eca08ab662955c4330f5c81fcc1b04d8ab
RyuChaehyeong <fluid15@naver.com>
2022-01-22 21:10:18 +0900

    [Design Pattern] Template Method

---
+++ src/templateMethod/StringDisplay.java
@@ -0,0 +1,38 @@
+package templateMethod;
+
+import java.nio.charset.StandardCharsets;
+
+public class StringDisplay extends AbstractDisplay{
+
+    private String str;
+    private int width;
+
+    public StringDisplay(String str) {
+        this.str = str;
+        this.width = str.getBytes(StandardCharsets.UTF_8).length;
+    }
+
+    @Override
+    public void open() {
+        printLine();
+    }
+
+    @Override
+    public void print() {
+        System.out.println("|" + str + "|");
+    }
+
+    @Override
+    public void close() {
+        printLine();
+    }
+
+
+    private void printLine() {
+        System.out.print("+");
+        for (int i=0; i < width; i++) {
+            System.out.print("-");
+        }
+        System.out.println("+");
+    }
+}
