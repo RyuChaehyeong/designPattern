513bf203659884dff30d0be1ded5abf33cd51b79
RyuChaehyeong <fluid15@naver.com>
2022-01-25 00:16:57 +0900

    [Design Pattern] Builder2

--- src/builder/TextBuilder.java
+++ src/builder/TextBuilder.java
@@ -1,6 +1,6 @@
 package builder;
 
-public class TextBuilder {
+public class TextBuilder extends Builder{
 
     private StringBuffer buffer = new StringBuffer();
     public void makeTitle(String title) {
@@ -9,5 +9,23 @@ public class TextBuilder {
         buffer.append("\n");
     }
 
+    public void makeString(String str) {
+        buffer.append("*" + str + "\n");
+        buffer.append("\n");
+    }
 
+    public void makeItems(String[] items) {
+        for (int i = 0; i < items.length; i++) {
+            buffer.append("  #" + items[i] + "\n");
+            buffer.append("\n");
+        }
+    }
+
+    public void close() {
+        buffer.append("===============\n");
+    }
+
+    public String getResult() {
+        return buffer.toString();
+    }
 }
