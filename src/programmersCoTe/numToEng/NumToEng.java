1025ae41e2ce439f5852f664e35f375a9f837df0
RyuChaehyeong <fluid15@naver.com>
2022-03-06 23:04:22 +0900

    [Practice Coding] Number to english

---
+++ src/programmersCoTe/numToEng/NumToEng.java
@@ -0,0 +1,16 @@
+package programmersCoTe.numToEng;
+
+public class NumToEng {
+
+    public int solution(String s) {
+        int answer = 0;
+        String[] str = {"zero","one", "two","three","four","five","six","seven","eight","nine"};
+        String[] num = {"0","1","2","3","4","5","6","7","8","9"};
+        for(int i=0; i<str.length; i++){
+            s = s.replace(str[i],num[i]);
+        }
+        answer = Integer.parseInt(s);
+        return answer;
+    }
+
+}
